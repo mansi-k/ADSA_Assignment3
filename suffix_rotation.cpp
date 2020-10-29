ab78011f5a1742a15d5a5d6c127d202d5e96b7b8
Mansi Khamkar <khamkarmansi@gmail.com>
2020-10-29 21:29:19 +0530

    Update suffix_rotation.cpp

--- suffix_rotation.cpp
+++ suffix_rotation.cpp
@@ -1,24 +1,11 @@
-//719
-//Glass Beads
-//Misc;String Matching;Suffix Array;Circular
 #include <iostream>
-#include <iomanip>
 #include <cstring>
 #include <string>
-#include <cmath>
-#define MAX 10050
 using namespace std;
-
-//int RA[6], tempRA[6];
-//int SA[6], tempSA[6];
-//int C[256];
-//int *RA, *tempRA;
-//int *SA, *tempSA;
-//int *C;
 
 
 void suffix_sort(int n, int k, int RA[], int SA[], int tempSA[] ) {
-    int C[256];
+    int C[max(256,n)];
     memset(C, 0, sizeof C);
 
     for (int i = 0; i < n; i++)
@@ -41,17 +28,8 @@ int suffix_array(string &s) {
     int n = s.size();
     int RA[n], tempRA[n];
     int SA[n], tempSA[n];
-//    RA = new int[n];
-//    RA = (int*) malloc(sizeof(int)*n);
-////    tempRA = new int[n];
-//    tempRA = (int*) malloc(sizeof(int)*n);
     memset(tempRA, 0, sizeof tempRA);
-////    SA = new int[n];
-//    SA = (int*) malloc(sizeof(int)*n);
-////    tempSA = new int[n];
-//    tempSA = (int*) malloc(sizeof(int)*n);
     memset(tempSA, 0, sizeof tempSA);
-//    C = new int[256];
 
     for (int i = 0; i < n; i++)
         RA[i] = s[i];
@@ -83,6 +61,5 @@ int main() {
     int pos = suffix_array(s);
     cout << pos+1 << endl;
     cout << s.substr(pos,s.length())+s.substr(0,pos) << endl;
-//    for(int i=0;i<MAX;i++)
-//        cout << i << "=" << C[i] << endl;
+    return 0;
 }
